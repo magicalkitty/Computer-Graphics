62acc0e45102c199c224d72214a5b35649c96329
Eva Natalía <evaelvars@gmail.com>
2026-03-02 14:09:05 -0600

    Removing shadow on NormalSHader

--- src/NormalShader.cpp
+++ src/NormalShader.cpp
@@ -3,9 +3,6 @@
 #include <algorithm>
 
 color NormalShader::rayColor(const Scene& world, const hit_record& hit, const Light& light, int depth) const {
-    if (world.shadowChecker(hit, 0.001, 1.0, light)) {
-        return color(0.0, 0.0, 0.0);
-    }
     
     return 0.5 * (hit.normal + color(1.0, 1.0, 1.0));
 }
