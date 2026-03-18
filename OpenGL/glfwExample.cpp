dc1f83e4f7a5556513d380055552c071e3a0c4a9
Eva Natalía <evaelvars@gmail.com>
2026-03-18 17:42:49 -0500

    Updating OpenGL

--- OpenGL/glfwExample.cpp
+++ OpenGL/glfwExample.cpp
@@ -61,7 +61,7 @@ int main(void)
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glClearColor(0.0, 0.7, 1.0, 1.0);
+    glClearColor(247.0/225.0, 136.0/255.0, 201.0/255.0, 1.0);
 
     int fb_width, fb_height;
     glfwGetFramebufferSize(window, &fb_width, &fb_height);
@@ -80,7 +80,57 @@ int main(void)
     std::cout << "GL_MAJOR_VERSION: " << major_version << std::endl;
 
     double timeDiff = 0.0, startFrameTime = 0.0, endFrameTime = 0.0;
+
+    // Initialize all my data and get it on the GPU.
+    // Load the scene file...
+    // Get my shapes in the scene...
+
+    GLuint m_triangleVBO[1], m_VAO;
+    sivelab::GLSLObject shader;
+
+    // Load a triangle
+    // create a Vertex Array Bugger to hold our triangle data
+
+    // create a Vertex Array Buffer to hold our triangle data                                               
+    glGenBuffers(1, m_triangleVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_triangleVBO[0]);
+
+    // this is the actual triangle data that will be copied to                                              
+    // the GPU memory                                                                                       
+    std::vector< float > host_VertexBuffer{ -0.5f, -0.5f, 0.0f,    // V0                                    
+                                            0.5f, -0.5f, 0.0f,    // V1                                    
+                                            0.0f, 0.5f, 0.0f };   // V2                                    
+
+    int numBytes = host_VertexBuffer.size() * sizeof(float);
+
+    // copy the numBytes from host_VertexBuffer t the GPU and store in                                      
+    // the currently bound VBO                                                                              
+    glBufferData(GL_ARRAY_BUFFER, numBytes, host_VertexBuffer.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // once copied, we no longer need the data on the host                                                  
+    host_VertexBuffer.clear();
+
+    // VAO for the VBO
+    // create a vertex array object that will map the attributes in                                         
+    // our vertex buffer to different location attributes for our                                           
+    // shaders                                                                                              
+    glGenVertexArrays(1, &m_VAO);
+    glBindVertexArray(m_VAO);
+
+    // VAO details here - we only have 1 attribute or location                                              
+    // (Position of the vertex)                                                                             
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, m_triangleVBO[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+    glBindVertexArray(0);
     
+    // Create a shader using my GLSLObject class                                                            
+    // sivelab::GLSLObject shader;
+    shader.addShader( "vertexShader_passthrough.glsl", sivelab::GLSLObject::VERTEX_SHADER );
+    shader.addShader( "fragmentShader_passthrough.glsl", sivelab::GLSLObject::FRAGMENT_SHADER );
+    shader.createProgram();
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
